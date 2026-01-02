008ed94de9630d19f2ac6e13e07c10c3b3f474c1
ll12375 <noneerrsomeok@gmail.com>
2026-01-02 21:56:49 +0700

    fix: allow serving js and css static assets by updating whitelist

--- lib/aether_web.ex
+++ lib/aether_web.ex
@@ -17,7 +17,7 @@ defmodule AetherWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt js css)
 
   def router do
     quote do
